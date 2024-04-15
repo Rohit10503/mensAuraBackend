a34127c384f8d89921e7366ae04807cab4dd881c
Rohit10503 <117336016+Rohit10503@users.noreply.github.com>
2024-04-15 16:42:38 +0530

    check

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
   // mongoose.connect("mongodb://127.0.0.1:27017/clothedb");
-  // mongoose.connect("mongodb+srv://rohitpandey10503:Rohit1234@cluster0.oknc0ab.mongodb.net/MensAura?retryWrites=true&w=majority&appName=Cluster0");
+  
 mongoose.connect("mongodb+srv://rohitpandey10503:Rohit1234@cluster0.oknc0ab.mongodb.net/MensAura?retryWrites=true&w=majority&appName=Cluster0",{
     useNewUrlParser:true,
  
