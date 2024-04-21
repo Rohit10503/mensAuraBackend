ca64c5105e75f2767465f1a5b7c09c36a770e227
Rohit10503 <117336016+Rohit10503@users.noreply.github.com>
2024-04-21 20:53:09 +0530

    add ml

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
   // mongoose.connect("mongodb://127.0.0.1:27017/clothedb");
-  
+//   mongodb+srv://rohitpandey10503:<password>@cluster0.oknc0ab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 mongoose.connect("mongodb+srv://rohitpandey10503:Rohit1234@cluster0.oknc0ab.mongodb.net/MensAura?retryWrites=true&w=majority&appName=Cluster0",{
     useNewUrlParser:true,
  
---
+++ ml_code/app.py
@@ -0,0 +1,40 @@
+# app.py
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the trained ML model
+new_df = joblib.load('product_dict.pkl')  #new_df is dictionary
+products = pd.DataFrame(new_df)
+similarity = joblib.load('similarity.pkl')
+# print(products['_id'].values)
+
+
+def recommendation(productId):
+  product_index = products[products["_id"] == productId].index[0]
+  distances = similarity[product_index]
+  products_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x: x[1])[20:35]
+  recommend_product = []
+  for i in products_list:
+    recommend_product.append(products.iloc[i[0]]._id)
+    # print(recommend_product)
+  return recommend_product
+
+
+# recommendation("66213cc8b41698340dff8532")
+
+
+@app.route('/predict/')
+@app.route('/predict/<prd_id>')
+def predict(prd_id=None):
+  rec=recommendation(prd_id)
+  print(rec)
+  return f"{prd_id}"
+    
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
